9ed286878f552afdc3ac47547619136f4bd74ab2
John Goetz <j.goetz@tecplot.com>
2023-09-04 16:27:06 -0700

    cleanup

--- build-vsgXchange.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HERE="$(realpath "$( dirname -- "${BASH_SOURCE[0]}" )" )"
-source ${HERE}/env.sh
-
-rm -fr $BUILD_DIR
-cmake -B ${BUILD_DIR}/vsgXchange -S ${TOPDIR}/vsgXchange -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX"
-cmake --build ${BUILD_DIR}/vsgXchange --target install
