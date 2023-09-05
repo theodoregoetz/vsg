0813c69f63de9f31539599a3bb68dadebe6c4af7
John Goetz <j.goetz@tecplot.com>
2023-09-04 19:08:15 -0700

    update build and conf scripts

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ if [[ $# -gt 1 ]]; then
     SUBMODULES="$1"
 fi
 
-for SUBMODULE in $SUBMODULES
+for SUBMODULE in "${SUBMODULES[@]}"
 do
     echo "building $SUBMODULE"
+    cmake --build ${BUILD_DIR}/$SUBMODULE
     cmake --build ${BUILD_DIR}/$SUBMODULE --target install
 done
--- config.sh
+++ config.sh
@@ -7,9 +7,9 @@ if [[ $# -gt 1 ]]; then
     SUBMODULES="$1"
 fi
 
-for SUBMODULE in $SUBMODULES
+for SUBMODULE in "${SUBMODULES[@]}"
 do
     echo "configuring $SUBMODULE"
-    rm -fr $BUILD_DIR
+    rm -fr $BUILD_DIR/$SUBMODULE
     cmake -B ${BUILD_DIR}/$SUBMODULE -S ${HERE}/$SUBMODULE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX"
 done
