c9f044e0bfa52cd801583e305b3c656aa6495c1f
John Goetz <j.goetz@tecplot.com>
2023-10-18 19:20:10 -0700

    update

--- config.sh
+++ config.sh
@@ -11,5 +11,10 @@ for SUBMODULE in "${SUBMODULES[@]}"
 do
     echo "configuring $SUBMODULE"
     rm -fr $BUILD_DIR/$SUBMODULE
-    cmake -B ${BUILD_DIR}/$SUBMODULE -S ${HERE}/$SUBMODULE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -DvsgXchange_all" -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX"
+    if [[ "$SUBMODULE" == "vsgXchange" ]]; then
+        CMAKE_OPTS="-DvsgXchange_assimp=FALSE"
+    elif [[ "$SUBMODULE" == "vsgExamples" ]]; then
+        CMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -DvsgXchange_all"
+    fi
+    cmake -B ${BUILD_DIR}/$SUBMODULE -S ${HERE}/$SUBMODULE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" "${CMAKE_OPTS[@]}" -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX"
 done
